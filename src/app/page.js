76ca11adb06fdbc5ddb02e29a259199cb7a27053
kocaman14 <o.kcmn@hotmail.com>
2024-09-13 22:53:44 +0300

    timeOut

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,102 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useState, useEffect } from "react";
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [click, setClick] = useState(false);
+  const [oldminute, setOldMinute] = useState(0);
+  const [oldsecond, setOldSecond] = useState(0);
+  const [minute, setMinute] = useState("");
+  const [second, setSecond] = useState("");
+  const [color, setColor] = useState(false);
+  const [reset,setReset] =useState(false)
+  const [button,setButton]=useState(null)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const handleMinute = (e) => {
+    setMinute(e.target.value);
+    setOldMinute(e.target.value);
+  };
+
+  const handleSecond = (e) => {
+    setSecond(e.target.value);
+    setOldSecond(e.target.value);
+  };
+  const startHandler = (e) => {
+    setClick(true);
+    setMinute(oldminute);
+    setSecond(oldsecond);
+    setReset(false)
+    setButton(false)
+  };
+  const pauseHandler = () => {
+    setClick((pre) => !pre);
+    setColor((pre) => !pre);
+    setButton(false)
+  };
+
+  const stopHandler = () => {
+    setClick(false);
+    setReset(true)
+    setButton(true)
+  };
+
+  useEffect(() => {
+    let timerId;
+    if (click) {
+      timerId = setTimeout(() => {
+        if (second > 0) {
+          setSecond((pre) => pre - 1);
+        } else if (second === 0 && minute > 0) {
+          setMinute((pre) => pre - 1);
+          setSecond(59);
+        } else if (minute === 0 && second === 0) {
+          setClick(false);
+        }
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [click, second, minute]);
+  const newColor = color ? "red" : "green";
+  const zeroNumber = minute < 10 && minute !== 10 ? "0" : "";
+  const seconZero = second < 10 && second !== 10 ? "0" : "";
+  return (
+    <div className="stopwatch-container">
+    <label> Minute:</label>
+
+      <input
+        type="number"
+        onChange={handleMinute}
+        value={minute}
+        placeholder={oldminute}
+        min="0"
+        max="59"
+        
+        />
+      <label>Second</label>
+      <input
+        type="number"
+        onChange={handleSecond}
+        value={second}
+        placeholder={oldsecond}
+        min="0"
+        max="59"
+      />
+      <button onClick={startHandler}>Start</button>
+      <button disabled={button} style={{ backgroundColor: newColor }} onClick={pauseHandler}>
+        Pause/Resume
+      </button>
+      <button onClick={stopHandler}>Reset</button>
+
+      <p>
+        {reset?"00:00":click
+          ? `${zeroNumber}${minute}:${seconZero}${second}`
+          : `${zeroNumber}${minute}:${seconZero}${second}`}
+       
+      </p>
+          </div>
+   
   );
 }
